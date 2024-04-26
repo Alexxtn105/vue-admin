22c81f184b2026dd7265361350251f1460a18cc3
Alexxtn105 <netmann105@gmail.com>
2024-04-26 16:40:58 +0300

    Настроил компоненты пользователей и график, прикрутил их в роутере

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,13 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 
-
+import DashboardItem from "@/pages/dashboard-item.vue";
+import UsersItem  from "@/pages/users-item.vue";
 const routes: Array<RouteRecordRaw> = [
-
+  {path:'', component:DashboardItem},
+  {path:'/users', component:UsersItem},
 ]
+
+//const router=createRouter(routes)
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
