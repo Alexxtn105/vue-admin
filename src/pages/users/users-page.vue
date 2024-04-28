f4804cb1fd57fdceb270e736a51bc751b21e684b
Alexxtn105 <133600113+Alexxtn105@users.noreply.github.com>
2024-04-28 19:00:58 +0300

    Добавил pagination для страницы пользователей

--- src/pages/users/users-page.vue
+++ src/pages/users/users-page.vue
@@ -1,4 +1,5 @@
 <template>
+  <!--  Таблица пользователей-->
   <div class="table-responsive small">
     <table class="table table-striped table-sm">
       <thead>
@@ -21,6 +22,18 @@
       </tbody>
     </table>
   </div>
+
+  <!--  Кнопки Предыдущая и Следующая-->
+  <nav>
+    <ul class="pagination">
+      <li class="page-item">
+        <a class="page-link" href="javascript:void(0)" @click="prev">Предыдущая</a>
+      </li>
+      <li class="page-item">
+        <a class="page-link" href="javascript:void(0)" @click="next">Следующая</a>
+      </li>
+    </ul>
+  </nav>
 </template>
 
 <!--Здесь используем сиснтаксис script setup, доступный в vue 3.-->
@@ -30,17 +43,46 @@ import {onMounted, ref} from "vue";
 import axios from "axios";
 
 //объявляем пустой массив для списка всех пользователей
-const users = ref([])
+const users = ref([]);
+const page = ref(1);
+const lastPage = ref(0);
 
-// выводим список всех пользователей
-onMounted(async () => {
-  const {data} = await axios.get('users');
+//функция подгрузки списка пользователей
+const load = async () => {
+  // вариант без pagination
+  // const {data} = await axios.get('users');
+  // вариант с pagination
+  const {data} = await axios.get(`users?page=${page.value}`);
 
   // Присваиваем влеичине users полученные из бэкенда данные
   // Здесь data.data означает то, что первая величина обеспечивается axios'ом,
-  // а вторая бэкендом
+  // а вторая бэкендом, так как мы используем pagination
   users.value = data.data;
-});
+
+  //берем индекс последней страницы из принятых данных, чтобы ограничить навигацию
+  lastPage.value = data.meta.last_page;
+}
+
+// выводим список всех пользователей
+onMounted(load);
+
+//добавляем постраничный вывод (pagination)
+//переход на предыдцщую страницу
+const prev = async () => {
+  if (page.value <= 1)
+    return
+  page.value--;
+  await load();
+};
+
+//переход на следующую страницу
+const next = async () => {
+  if (page.value > lastPage.value)
+    return;
+
+  page.value++;
+  await load();
+};
 
 </script>
 
